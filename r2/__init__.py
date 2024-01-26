4c1cc94436fd5f9eb3c2dab4c379b8419c00e696
iresharma <iresh.sharma8@gmail.com>
2024-01-26 22:35:17 +0530

    create folder

--- r2/__init__.py
+++ r2/__init__.py
@@ -1,10 +1,6 @@
 import os
 from R2Wrapper import R2Wrapper
 
-r2 = R2Wrapper(os.environ['ENDPOINT'], os.environ['KEY_ID'], os.environ['SECRET_KEY'], 'apac')
-r2.list_bucket('temp-reach-bucket', '/')
-# r2.init_bucket('123', {
-#     'accountId': '123',
-#     'name': 'Shreesh',
-#     'email': 'sgkul2000@gmail.com'
-# })
+if __name__ == '__main__':
+    r2 = R2Wrapper(os.environ['ENDPOINT'], os.environ['KEY_ID'], os.environ['SECRET_KEY'], 'apac')
+    r2.create_folder('temp-reach-bucket', '/wow')
